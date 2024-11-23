938905692142daabdbbec2e6f605cf44937aca63
Diego Ramon <61364290+devdiegoramon@users.noreply.github.com>
2024-11-23 11:59:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,46 +2,46 @@ import time
 from utilities.clear import clear
 from modules.pc import Pc
 from calc.ligarpc import ligarPc
-# from calc.reportar import reportar  # Descomente se for necessário
 
 class Program:
-    def __init__(this):
-        this.dia = 0
-        this.minutos = 0
-        this.horas = 0
-        this.timerate = 0.5
-        this.lista_manutencao = []
-        # Lista de computadores
-        this.pcs = [
+    def __init__(self, callback):
+        self.dia = 0
+        self.minutos = 0
+        self.horas = 0
+        self.timerate = 0.5
+        self.lista_manutencao = []
+        self.pcs = [
             Pc("Maquina 1"),
             Pc("Maquina 2"),
             Pc("Maquina 3")
         ]
+        self.callback = callback
 
+    def iniciar(self):
         while True:
             clear()
             # Printar dia, data e hora no terminal
-            print(f"Dia {this.dia:02} | {this.horas:02}h{this.minutos:02}")
-            ligarPc(this.pcs)
-            this.listar_pcs()
-            time.sleep(this.timerate)
-            this.minutos += 1
-            if this.minutos == 60:
-                this.minutos = 0
-                this.horas += 1
-            if this.horas == 8:
-                this.horas = 0
-                this.dia += 1
+            print(f"Dia {self.dia:02} | {self.horas:02}h{self.minutos:02}")
+            ligarPc(self.pcs)
+            self.listar_pcs()
+            self.callback(self.dia, self.horas, self.minutos, self.pcs)  # Atualizar a interface gráfica
+            time.sleep(self.timerate)
+            self.minutos += 1
+            if self.minutos == 60:
+                self.minutos = 0
+                self.horas += 1
+            if self.horas == 8:
+                self.horas = 0
+                self.dia += 1
 
-    def listar_pcs(this):
-        for pc in this.pcs:
+    def listar_pcs(self):
+        for pc in self.pcs:
             print(f"\nNome: {pc.id} | Ligado: {pc.ligado}\nComponentes: ")
             for computador in pc.componentes:
                 print(f"Nome: {computador.id} | Quebrado: {computador.quebrado} | Tempo de vida: {computador.tempo_de_vida:.2f} | Custo: {computador.custo}")
                 if computador.quebrado:
                     pc.ligado = False
                 else:
-                    # Computador Não Está quebrado
                     if pc.ligado and computador.tempo_de_vida > 0:
                         computador.vidautil()
                     if computador.tempo_de_vida <= 0:
@@ -49,5 +49,3 @@ class Program:
                     elif computador.tempo_de_vida <= 0:
                         computador.quebrado = True
                         pc.ligado = False
-
-program = Program()
