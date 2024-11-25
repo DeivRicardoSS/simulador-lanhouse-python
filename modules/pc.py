2f6c2bd275ad8294c92f73e5d6e0e5cc73c71930
devdiegoramon <brilhocelestebc15@gmail.com>
2024-11-25 09:03:03 -0300

    criado requirements.txt

--- modules/pc.py
+++ modules/pc.py
@@ -1,14 +1,26 @@
-from modules.computador import Computador 
+from modules.computador import Computador
 
 class Pc:
     def __init__(this, id):
+
         this.id = id
         this.ligado = False
         this.status = "Funcionando"
-        this.componentes = [ 
+
+        this.componentes = [
             Computador("cpu"),
             Computador("memoria"),
             Computador("placa_mae"),
             Computador("memoria_ram"),
             Computador("fonte")
         ]
+
+class Componente:
+    def __init__(self, id):
+        self.id = id
+        self.quebrado = False
+        self.tempo_de_vida = 100
+        self.custo = 50
+
+    def vidautil(self):
+        self.tempo_de_vida -= 0.5
--- utilities/clear.py
+++ utilities/clear.py
@@ -1,3 +1,9 @@
 import os
+import platform
+
 def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    system = platform.system()
+    if system == "Windows":
+        os.system("cls")
+    else:
+        os.system("clear")
